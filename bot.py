08293ace3651d1a1dd055485046e47027ca946a1
h0wser <evilhowser@gmail.com>
2014-04-17 16:48:20 +0200

    Added basic chat command responses to the config file. Implemented text command responses in the bot

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import json
 
 class SugBot:
 	def __init__(self, cfgfile):
-		self.loadconfig(cfgfile)
+		self.load_config(cfgfile)
 
-	def loadconfig(self, cfgfile):
+	def load_config(self, cfgfile):
 		print "Loading config"
 		#open and parse json file
 		cf = open(cfgfile, "r")
@@ -14,6 +14,7 @@ class SugBot:
 
 		# assign stuff according to config
 		self.chatname = config['chatname']
+		self.commands = config['commands']
 
 	def start(self):
 		print "Starting bot"
@@ -21,4 +22,21 @@ class SugBot:
 		self.skype.Attach()
 
 		self.chat = self.skype.Chat(self.chatname)
-		self.chat.SendMessage("I'm in dude")
+		self._setupEventHandlers()
+
+	def _setupEventHandlers(self):
+		self.skype.RegisterEventHandler('MessageStatus', self.message_handler)
+
+	def message_handler(self, Message, Status):
+		if (Message.Chat == self.chat) and (Status != Skype4Py.cmsSending):
+			if (Message.Body[0] == '!'):
+				self._parse_command(Message.Body)
+
+	def _parse_command(self, command):
+		index = 0
+		for i in range(0, len(self.commands)):
+			if (command == self.commands[i]['chat_command']):
+				index = i
+				break
+
+		self.chat.SendMessage(self.commands[i]['response'])
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import bot
 def main(argv):
 	sugbot = bot.SugBot("config.json")
 	sugbot.start()
+	while True:
+		pass
 
 if __name__ == "__main__":
 	main(sys.argv)
